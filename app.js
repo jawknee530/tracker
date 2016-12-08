8e8515e9b35f41259cf1aae3e8afab49ff2ada0c
Ubuntu <ubuntu@ip-172-31-33-231.us-west-2.compute.internal>
2016-12-08 07:30:04 +0000

    just work all the time please

--- app.js
+++ app.js
@@ -23,29 +23,78 @@ var findID = function(data, qPort, qip) {
   return false;
 }
 
+var findHash = function(data, id) {
+  for(var i = 0; i < data.length; i++) {
+    if(data[i].peer_id === id) {
+      return data[i];
+    }
+  }
+  return false;
+}
+
+function hashy (str) {
+  var url = str;
+  var hexval = '';
+
+  for(var i = 0; i < url.length; i++) {
+    if(url[i] !== '%') {
+      var code = url.charCodeAt(i);
+      var hex = code.toString(16);
+      hexval += hex;
+    } else {
+      hexval += url[i+1] + url[i+2];
+      i += 2;
+    }
+  }
+  return hexval;
+}
 
 app.get('/', function(req, res) {
   //data.test = req.query.test;
-  //console.log(req.query);
+  //var info_hash = decodeURIComponent(req.query.info_hash);
+  //console.log(info_hash);
   //data.info_hash = req.query.info_hash;
   //data.info_hash.ip = req.connection.remoteAddress;
   //data.info_hash.port = req.query.port;
-  var torrent = findTorrent(data, req.query.info_hash);
+  //console.log(ip);
+
+  var info_hash = hashy(req.query.info_hash);
+  console.log(info_hash);
+  var peer_id = decodeURI(req.query.peer_id);
+
+  var ip = req.connection.remoteAddress;
+  if(ip.substring(0,7) == '::ffff:') {
+    ip = ip.substring(7);
+  }
+  var port = req.connection.remotePort;
+  //var port = req.query.port;
+  console.log(ip);
+  var torrent = findTorrent(data, info_hash);
   if (torrent === false){
-    var obj = {"info_hash" : req.query.info_hash, "peers" : [{ "peer_id" : req.query.peer_id, "ip" : req.connection.remoteAddress, "port" : req.connection.remotePort}]};
+    var obj = { "info_hash" : info_hash, "peers" : [{ "peer_id" : peer_id, "ip" : ip, "port" : port }]};
     data.push(obj);
     torrent = obj;
     //console.log(obj.peers);
   }
   else {
-    var peer = findID(torrent.peers, req.query.port, req.connection.remoteAddress);
+    //var peer = findID(torrent.peers, port, ip);
+    var peer = findHash(torrent.peers, peer_id);
     if(peer === false){
-      var obj = { "peer_id" : req.query.peer_id, "ip" : req.connection.remoteAddress, "port" : req.connection.remotePort };
+      var obj = { "peer_id" : peer_id, "ip" : ip, "port" : port };
       torrent.peers.push(obj);
     }
+    else {
+      peer.ip = ip;
+      peer.port = port;
+    }
   }
-
-  var response = bencode(torrent);
+  
+  if(torrent) {
+    var response = bencode(torrent);
+  }
+  else {
+    response = 'error';
+  }
 
   //console.log(data);
 
@@ -54,22 +103,24 @@ app.get('/', function(req, res) {
 });
 
 var bencode = function(torrent) {
-  var response = 'd8:intervali120e5:peersld'
+  var response = 'd8:intervali600e5:peersl'
   for(var i = 0; i < torrent.peers.length; i++) {
-    response = response.concat('7:peer id');
-    response = response.concat(torrent.peers[i].peer_id.length + ':');
-    response = response.concat(torrent.peers[i].peer_id);
+    response = response.concat('d');
     response = response.concat('2:ip');
     response = response.concat(torrent.peers[i].ip.length + ':');
     response = response.concat(torrent.peers[i].ip);
+    //response = response.concat('7:peer id');
+    //response = response.concat(torrent.peers[i].peer_id.length + ':');
+    //response = response.concat(torrent.peers[i].peer_id);
     response = response.concat('4:port');
     response = response.concat('i' + torrent.peers[i].port + 'e');
+    response = response.concat('e');
   }
-  response = response.concat('eee');
-  //console.log(response);
+  response = response.concat('ee');
+  console.log(response);
   return response;
 }
 
-app.listen(3000, function() {
-  console.log('Example app listening on port 3000!');
+app.listen(4000, function() {
+  console.log('Example app listening on port 4000!');
 });
